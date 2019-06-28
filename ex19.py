09f6eaadcdd6df05d339607ccaf3e22b2b64308a
danialhx <hussin.danial@gmail.com>
2019-06-28 13:15:12 +0800

    Update ex19.py
    topup comments

--- ex19.py
+++ ex19.py
@@ -1,20 +1,26 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
+#defining the function
         print "You have %d cheeses!" % cheese_count
-        print "You have $d boxes of crackers!" % boxes_of_crackers
+        print "You have %d boxes of crackers!" % boxes_of_crackers
         print "Man that's enough for a party!"
         print "Get a blanket. \n"
 
+#defining different arguments for the function
 print "We can just give the function numbers directly:"
-cheese_and_crackers(20,30)
+cheese_and_crackers(20, 30)
 
-print "Or, we can use variables from our script:"
+print "Or, we can use variables from our script:"   
+#adjusting the argv in the function
 amount_of_cheese = 10
 amount_of_crackers = 50
 
+#new def with new args
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
+#args not used, but utilise numbers
 print "We can even do math inside too:"
 cheese_and_crackers(10 + 20, 5 + 6)
 
+#using args and some numbers
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
