3f775384f6112537ebbeb9f2b33478b927d9067a
danialhx <hussin.danial@gmail.com>
2019-06-28 12:56:56 +0800

    Add files via upload

--- ex18.py
+++ ex18.py
@@ -1,14 +1,14 @@
 # this one is like your scripts with argv
-def print_two(*args):
-        arg1, arg2 = args
+def print_two(*args):                   
+        arg1, arg2 = args               #taking two argv1, argv2 into a args
         print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 #ok, that *args is actually pointless, we can just do this
-def print_two_again(arg1, arg2):
+def print_two_again(arg1, arg2):        #same with above
         print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # this just takes on argument
-def print_one(arg1):
+def print_one(arg1):                    
         print "arg1: %r" % arg1
 
 # this one takes no arguments
@@ -19,3 +19,5 @@ print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
 print_one("First!")
 print_none()
+
+#to build functions. 
