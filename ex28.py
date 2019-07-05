8b38464974672439689fb8d253b712fc7f47b692
danialhx <hussin.danial@gmail.com>
2019-07-05 16:32:17 +0800

    Add files via upload

---
+++ ex28.py
@@ -0,0 +1,47 @@
+q1 = True and True
+q2 = False and True
+q3 = 1 == 1 and 2 == 1
+q4 = "test" == "test" 
+q5 = 1 == 2 or 2 != 1
+q6 = True and 1 == 1
+q7 = False and 0 != 0
+q8 = True or 1 == 1
+q9 = "test" == "testing"
+q10 = 1 != 0 and 2 == 1
+q11 = "test" != "testing"
+q12 = "test" == 1
+q13 = not (True and False)
+q14 = not (1 == 1 and 0 != 1)
+q15 = not (10 == 1 or 1000 == 1000)
+q16 = not (1 != 10 or 3 == 4)
+q17 = not ("testing" == "testing" and "Zed" == "Cool Guy")
+q18 = 1 == 1 and not ("testing" == 1 or 1 == 0) 
+q19 = "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+q20 = 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+
+#questions = [q1,q2,q3]
+
+#for question in questions:
+#        print question
+
+print(q1) #True.
+print(q2) #False.
+print(q3) #False.
+print(q4) #True.
+print(q5) #True.
+print(q6) #True.
+print(q7) #False.
+print(q8) #True.
+print(q9) #False.
+print(q10) #False.
+print(q11) #True.
+print(q12) #False.
+print(q13) #True.
+print(q14) #False.
+print(q15) #False.
+print(q16) #False
+print(q17) #True.
+print(q18) #True.
+print(q19) #False.
+print(q20) #False.
+
