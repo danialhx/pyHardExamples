d9831174f4f0d718ed1f993a0ea858d01e8f5fae
danialhx <hussin.danial@gmail.com>
2019-07-12 23:30:12 +0800

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,12 @@
+animals = ['bear', 'python', 'peacock','kangaroo', 'whale','platypus']
+
+print('1. The animal at 1 is %r.' % animals[0])
+print('The third animal is %r.' % animals[2])
+print('The first animal is %r.' % animals[0])
+print('The animal at 3 is %r.' % animals[3])
+print('The fifth animal is %r.' % animals[4])
+print('The animal at 2 is %s.' % animals[1])
+print('The sixth animal is %s.' % animals[5])
+print('The animal at 4 is %r.' % animals[4])
+
+#programmed the questions to show the answer, but manually writing it according to the list.
